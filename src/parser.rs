338e5c0bf2007c79f62a2f7a880f1d1448a3060d
Jacek Przemieniecki <github@przemieniecki.net>
2016-01-25 23:15:40 +0100

    Added AST generation

--- src/parser.rs
+++ src/parser.rs
@@ -107,16 +107,135 @@ fn reverse_and_sort(state_sets: &Vec<Vec<EarleyItem>>, gram: &Grammar) -> Vec<Ve
     for _ in 0..state_sets.len() {
         result.push(Vec::new());
     }
-    for (i, state_set) in state_sets.iter().enumerate() {
+    for (i, state_set) in state_sets.iter().rev().enumerate() {
+        let bin = state_sets.len() - i - 1;
         for item in state_set.iter().filter(|i| i.is_complete(&gram)) {
             let start = item.start;
-            let copy = EarleyItem {rule_id: item.rule_id, position: item.position, start: i};
+            let copy = EarleyItem {rule_id: item.rule_id, position: item.position, start: bin};
             result[start].push(copy);
         }
     }
     return result;
 }
 
+fn print_state_sets(state_sets: &Vec<Vec<EarleyItem>>, gram: &Grammar, tokens: &Vec<Token>, print_tokens: bool) {
+    for (set_index, set) in state_sets.iter().enumerate() {
+        for item in set {
+            println!("{}", item.to_string(&gram));
+        }
+        if set_index < tokens.len() {
+            if print_tokens {
+                println!("{}: {}", set_index, tokens[set_index].text);
+            }
+            else {
+                println!("{}", set_index);
+            }
+        }
+    }
+}
+
+struct ASTNode {
+    children: Vec<ASTNode>,
+    value: usize
+}
+
+fn find_split<'a>(
+    from: usize, 
+    to: usize, 
+    symbols: &[usize], 
+    lookup_sets: &'a Vec<Vec<EarleyItem>>, 
+    gram: &Grammar
+    ) -> Option<Vec<&'a EarleyItem>> {
+
+    if from == to && symbols.len() == 0 {
+        return Some(Vec::new());
+    }
+    let (symbol, rest) = match symbols.split_first() {
+        Some((x, y)) => (x, y),
+        None => return None
+    };
+    for item in lookup_sets[from].iter().filter(
+            |e| e.start <= to && &gram.rules[e.rule_id].start == symbol
+        ) {
+        match find_split(item.start, to, rest, &lookup_sets, &gram) {
+            Some(mut v) => { v.push(item); return Some(v);}
+            None => {}
+        }
+    }
+    return None;
+}
+
+fn build_ast_internal(
+    from: usize, 
+    split: &Vec<&EarleyItem>,
+    lookup_sets: &Vec<Vec<EarleyItem>>, 
+    gram: &Grammar) -> Option<Vec<ASTNode>> {
+
+    let mut ast_children = Vec::new();
+    let mut inner_from = from;
+    for item in split.iter().rev() {
+        let child = match build_ast(inner_from, item.start, gram.rules[item.rule_id].start, &lookup_sets, &gram) {
+            Some(sub_tree) => sub_tree,
+            None => return None
+        };
+        ast_children.push(child);
+        inner_from = item.start;
+    }
+    return Some(ast_children);
+}
+
+fn build_ast(
+    from: usize, 
+    to: usize, 
+    root_symbol: usize, 
+    lookup_sets: &Vec<Vec<EarleyItem>>, 
+    gram: &Grammar) -> Option<ASTNode> {
+    
+    if gram.symbols[root_symbol].is_terminal {
+        return Some(ASTNode {children: Vec::new(), value: root_symbol});
+    }
+    let edges : Vec<_> = lookup_sets[from].iter().filter(|item| item.start == to ).collect();
+    if edges.is_empty() {
+        return None;
+    }
+
+    for edge in edges {
+        if gram.rules[edge.rule_id].symbols.iter().all(|i| gram.symbols[i.clone()].is_terminal) {
+            let mut children = Vec::new();
+            for terminal in &gram.rules[edge.rule_id].symbols {
+                children.push(ASTNode {children: Vec::new(), value: terminal.clone()});
+            }
+            return Some(ASTNode {children: children, value: root_symbol});
+        }
+        let split = match find_split(from, to, &gram.rules[edge.rule_id].symbols[..], &lookup_sets, &gram) {
+            Some(x) => x,
+            None => continue
+        };
+        // we found a split, let's try to make a parse tree out of it
+        let ast_children = match build_ast_internal(from, &split, &lookup_sets, &gram) {
+            Some(x) => x,
+            None => continue
+        };
+        return Some(ASTNode {children: ast_children,  value: root_symbol});
+    }
+    return None
+}
+
+impl ASTNode {
+    fn to_string(&self, gram: &Grammar, indent: &str, last: bool) -> String {
+        let mut s = String::new();
+        write!(s, "{}", indent);
+        let new_indent = match last {
+            true => { write!(s, "\\-"); indent.to_string() + "  "},
+            false => { write!(s, "|-"); indent.to_string() + "| "}
+        };
+        write!(s, "{}\n", &gram.symbols[self.value].name);
+        for (i, node) in self.children.iter().enumerate() {
+            write!(s, "{}", node.to_string(&gram, &new_indent, i == self.children.len() -1));
+        }
+        return s;
+    }
+}
 pub fn parse(gram: Grammar, tokens: Vec<Token>) {
     let mut state_sets = Vec::new();
     
@@ -150,14 +269,14 @@ pub fn parse(gram: Grammar, tokens: Vec<Token>) {
 
     let reversed = reverse_and_sort(&state_sets, &gram);
 
+    let ast = match build_ast(0, tokens.len()-1, gram.start, &reversed, &gram) {
+        Some(x) => x,
+        None => panic!("Failed to build ast")
+    };
     println!("{}", gram);
 
-    for (set_index, set) in state_sets.iter().enumerate() {
-        for item in set {
-            println!("{}", item.to_string(&gram));
-        }
-        if set_index < tokens.len() {
-            println!("{}: {}", set_index, tokens[set_index].text);
-        }
-    }
+    print_state_sets(&state_sets, &gram, &tokens, true);
+
+    println!("\n\n");
+    println!("{}", ast.to_string(&gram, "", true));
 }
